23c284ad9701ac241a070e3e7998824943a49722
Dedexz R Comotecyn <jengkeran@gmail.com>
2021-07-04 10:50:22 +0700

    Create blogaktiv.js
    siap tempur

---
+++ blogaktiv.js
@@ -0,0 +1,41 @@
+var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://www.alternativecpmgate.com/urtaukg1ib?key=e86e8c2cd3d7d72acf189455cb27ae74"
+                        ]);
+
+    var ars             = rChoice([
+                            "https://www.cuteabis.com"
+                        ]);
+
+
+    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
