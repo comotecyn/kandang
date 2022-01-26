7906f68ae079f8e7c80db7f4a62981bc57c33fc8
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-01-26 11:39:13 +0700

    Create ddstera.js
    simpan

---
+++ ddstera.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '1c1ef7c23b87379cfde22b254e773e25',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/1c1ef7c23b87379cfde22b254e773e25/invoke.js"></scr' + 'ipt>');
+</script>
