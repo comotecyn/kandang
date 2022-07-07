8d6f2e470ae0877fce6d3c4664a435e6db7e0549
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-07-07 07:22:48 +0700

    Update ddstera.js
    tanba js

--- ddstera.js
+++ ddstera.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'f4156a5a883dd9c6890bbb92773f99dc',
 		'format' : 'iframe',
@@ -7,4 +7,4 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplayformat.com/f4156a5a883dd9c6890bbb92773f99dc/invoke.js"></scr' + 'ipt>');
-</script>
+
