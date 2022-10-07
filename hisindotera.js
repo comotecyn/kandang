3aac69bd3cfde77590ed2c25692f33810e8c5913
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-10-07 20:44:25 +0700

    Create hisindotera.js

---
+++ hisindotera.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : 'f4156a5a883dd9c6890bbb92773f99dc',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/f4156a5a883dd9c6890bbb92773f99dc/invoke.js"></scr' + 'ipt>');
