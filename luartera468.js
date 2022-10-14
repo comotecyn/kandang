211b1d2be8da464cfc9c6561075c2b1dbaca0a46
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-10-14 10:08:27 +0700

    Create luartera468.js

---
+++ luartera468.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'c4717464f385d69cdd57d003042d97cd',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/c4717464f385d69cdd57d003042d97cd/invoke.js"></scr' + 'ipt>');
