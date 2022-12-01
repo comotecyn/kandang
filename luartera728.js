54f5024591d3e7876b796c3da4b8a9f64f5642e3
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-02 06:58:38 +0700

    Create luartera728.js

---
+++ luartera728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '430b8e325accb4099ace3419544a079b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/430b8e325accb4099ace3419544a079b/invoke.js"></scr' + 'ipt>');
