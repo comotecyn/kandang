4e035dccc59724971957f1a9c13a62fe9b81af30
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-08 09:44:37 +0700

    Create luartera300.js

---
+++ luartera300.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '1c1ef7c23b87379cfde22b254e773e25',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/1c1ef7c23b87379cfde22b254e773e25/invoke.js"></scr' + 'ipt>');
