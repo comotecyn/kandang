e2d793f42c9f43c553dcaf75857469a1cec2333a
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-08 09:35:38 +0700

    Create popjos.js

---
+++ popjos.js
@@ -0,0 +1,12 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://www.highperformancecpmgate.com/xe9ywmt8d?key=bab4804d69366375fc41ad51af50f9ec', '_blank');
+	});
