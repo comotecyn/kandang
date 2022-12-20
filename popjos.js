e0f3e6705dd4435222c3b9ea9a6807814ab5335a
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-20 20:41:42 +0700

    Update popjos.js

--- popjos.js
+++ popjos.js
@@ -8,5 +8,5 @@
 	    $(this).removeClass('xepo_ads');
 
 	    //ubah google dengan ads direct link
-	    window.open('https://www.highperformancecpmgate.com/xe9ywmt8d?key=bab4804d69366375fc41ad51af50f9ec', '_blank');
+	    window.open('https://www.highperformancecpmgate.com/ymkq6d6p?key=187493c8159e3934f28c7782b0f1cf04', '_blank');
 	});
