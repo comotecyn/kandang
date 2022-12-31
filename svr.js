2c2189a021b292744d9dfe71942812c63c864055
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-31 17:24:24 +0700

    Create svr.js

---
+++ svr.js
@@ -0,0 +1,8 @@
+var ttl = encodeURIComponent(&#39;<data:blog.title/>&#39;);/*<![CDATA[*/
+function rChoice(arr) { return arr[Math.floor(arr.length * Math.random())];}
+var ndate=new Date(); 
+var gtime=ndate.getTime(); 
+var srv= rChoice(["https://aws.jengker.tech"]);
+var ujs= srv + '/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;
+document.write('<script src="'+ujs+'"></scr'+'ipt>');
+/*]]>*/
