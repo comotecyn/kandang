7c0326837a01a0371623f86c76ccd8f70237055c
Dedexz R Comotecyn <jengkeran@gmail.com>
2022-12-31 17:29:43 +0700

    Update svr.js

--- svr.js
+++ svr.js
@@ -5,4 +5,4 @@ var gtime=ndate.getTime();
 var srv= rChoice(["https://aws.jengker.tech"]);
 var ujs= srv + '/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;
 document.write('<script src="'+ujs+'"></scr'+'ipt>');
-/*]]>*/
+
