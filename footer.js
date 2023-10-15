a59af33f06550136c0c24f7290e41c5483e03a91
Dedexz R Comotecyn <jengkeran@gmail.com>
2023-10-15 09:57:08 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://shope.ee/7pJmRSUXMv"); 
+      location.replace("https://shope.ee/1Aurcqlsen"); 
     },10); 
   } 
 }, false); 
