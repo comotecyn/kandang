788fe4a6886fc16107edb99443d8873619c5d694
Dedexz R Comotecyn <jengkeran@gmail.com>
2024-09-29 17:39:36 +0700

    Update hisindotera.js

--- hisindotera.js
+++ hisindotera.js
@@ -6,4 +6,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/f4156a5a883dd9c6890bbb92773f99dc/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/f4156a5a883dd9c6890bbb92773f99dc/invoke.js"></scr' + 'ipt>');
