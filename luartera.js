920402fc83a7c9ddc297230d5da3350aad5b8ecf
Dedexz R Comotecyn <jengkeran@gmail.com>
2025-05-16 19:08:31 +0700

    Update luartera.js

--- luartera.js
+++ luartera.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/1c1ef7c23b87379cfde22b254e773e25/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitableratecpm.com/1c1ef7c23b87379cfde22b254e773e25/invoke.js"></scr' + 'ipt>');
